2c2cb24bd3d60565119289644f402e1c763c6f2c
KyleTryon <Kyle.Tryon@circleci.com>
2022-11-18 09:59:56 -0500

    refactor: cleanup install script

--- src/scripts/install-bats.sh
+++ src/scripts/install-bats.sh
@@ -1,34 +1,40 @@
 #!/bin/bash
+
 checkRequirements() {
-    if ! which git > /dev/null; then
+    if ! command -v git &> /dev/null
+    then
         echo "git is required to install BATS"
         exit 1
     fi
+    # TODO: revisit if SUDO is needed. Keeping for patch compatability
     if [[ $EUID == 0 ]]; then export SUDO=""; else export SUDO="sudo"; fi
 }
 
 installBats() {
+    checkRequirements
     cd /tmp || exit 1
     git clone https://github.com/bats-core/bats-core.git
     cd /tmp/bats-core || exit 1
     $SUDO ./install.sh /usr/local
-}
-
-
-checkInstall() {
-    if ! which bats > /dev/null; then
-        echo "BATS has failed to install."
+    if ! command -v bats &> /dev/null
+    then
+        echo "BATS failed to install"
+        echo "Please open an issue at: https://github.com/CircleCI-Public/BATS-orb/issues"
         exit 1
+    else
+        echo "BATS installed successfully"
     fi
-    echo
-    echo "BATS-Core installed"
-    echo
 }
 
-runInstallBats() {
-    checkRequirements
-    installBats
-    checkInstall
+setupBats() {
+    if ! command -v bats &> /dev/null 
+    then
+    # BATS is not installed
+        installBats
+    else
+    # BATS is installed
+        echo "BATS is already available"
+    fi
 }
 
-runInstallBats
+setupBats
